b1c8bdb141f13a05e58549dfd40ff6e9b49391fc
Fiveolioo <96richardohc@gmail.com>
2020-06-19 21:37:23 -1000

    getting API Data

--- lib/API.rb
+++ lib/API.rb
@@ -1,10 +1,14 @@
 class StarwarsCharacters::API
 
-    BASE_URL = "https://swapi.dev/"
+    BASE_URL = "https://swapi.dev/api/"
     
 
     def self.get_characters
       puts "MAKING A NETWORK REQUEST"
+
+      url = BASE_URL + "people"
+      res = HTTParty.get(url)
+      res 
     end
 
 end
